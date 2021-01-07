28fecbff57d27d4e9e44508ecd0a87c02a5e9f1e
robstaples <rob.staples91@gmail.com>
2021-01-07 12:53:51 +1100

    adding interaces

---
+++ src/lib/api/types.ts
@@ -0,0 +1,36 @@
+export interface CharectarResponse {
+  levelzero: LevelZeroResponse;
+  attributes: AttributeListResponse;
+}
+
+export interface LevelZeroResponse {
+  name: string;
+  ancestery: string;
+  background: string;
+  class: string;
+  experience: number;
+}
+
+export interface AttributeListResponse {
+  str: AttributeResponse;
+  dex: AttributeResponse;
+  con: AttributeResponse;
+  int: AttributeResponse;
+  wis: AttributeResponse;
+  cha: AttributeResponse;
+}
+
+export interface AttributeResponse {
+  base: number;
+  modifier: number;
+}
+
+export interface SkillListResponse {
+  acrobatics: SkillListResponse;
+}
+
+export interface SkillResponse {
+  skill: string;
+  attr: AttributeResponse;
+  proficiency: number;
+}
