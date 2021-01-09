d96b5842ab9bcf0086d6c0bbeb9f894ca4602cfe
robstaples <rob.staples91@gmail.com>
2021-01-09 11:31:33 +1100

    adding savingthrows and ac

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import AttributeHotbar from  './components/AttributeHotbar/AttributeHotbar';
-import HealthBar from  './components/HealthBar/HealthBar';
-import LevelZero from  './components/LevelZero/LevelZero';
-import Skills from  './components/Skills/Skills';
-
+import AttributeHotbar from './components/AttributeHotbar/AttributeHotbar';
+import Ac from './components/AC/ArmorClass';
+import HealthBar from './components/HealthBar/HealthBar';
+import LevelZero from './components/LevelZero/LevelZero';
+import Skills from './components/Skills/Skills';
+import SavingThrows from './components/SavingThrows/SavingThrows';
 
 ReactDOM.render(
   <div>
     <React.StrictMode>hello there</React.StrictMode>
     <HealthBar />
+    <Ac />
     <AttributeHotbar />
     <LevelZero />
+    <SavingThrows />
     <Skills />
   </div>,
   document.getElementById('root')
